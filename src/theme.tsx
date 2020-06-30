709647dc7d10cd518f3bf6c158ffa7f6affca78b
Gavin <gavin.lui@gmail.com>
2020-06-30 23:49:27 +1000

    Applied styling to name
    cleaned out public folder, added in Archivo Narrow font, applied font  and primary color to Name

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,5 +1,12 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
+// const archivoNarrow = {
+//   fontFamily: "Archivo Narrow",
+//   fontStyle: "normal",
+//   fontWeight: 100,
+//   src: `url(${ArchivoNarrow})`,
+// };
+
 // A custom theme for this app
 const theme = createMuiTheme({
   palette: {
@@ -12,6 +19,9 @@ const theme = createMuiTheme({
     background: {
       default: "#fff",
     },
+  },
+  typography: {
+    fontFamily: ["Archivo Narrow", "Roboto", "sans-serif"].join(","),
   },
 });
 
