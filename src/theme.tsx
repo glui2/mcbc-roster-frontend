95a3e279ef8c34d214ed8769dbe49af83db06c4a
Gavin <gavin.lui@gmail.com>
2020-07-07 22:37:11 +1000

    removed comments from theme.tsx

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,12 +1,5 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
-// const archivoNarrow = {
-//   fontFamily: "Archivo Narrow",
-//   fontStyle: "normal",
-//   fontWeight: 100,
-//   src: `url(${ArchivoNarrow})`,
-// };
-
 // A custom theme for this app
 const theme = createMuiTheme({
   palette: {
